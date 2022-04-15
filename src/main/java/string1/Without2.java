aac31cd74de54a8fdfcc6eba963bc50c7b6c33d7
michaelWlodarczak <michael.wlodarczak@gmail.com>
2022-04-15 13:31:53 +0200

    'without2' completed, all package 'string1' completed, some todo's left in the meantime

--- src/main/java/string1/Without2.java
+++ src/main/java/string1/Without2.java
@@ -12,43 +12,27 @@ without2("Hi") → ""
  */
 
 public class Without2 {
-//    public String without2(String str) {
-//        if(str.length()<=2){
-//            return "";
-//        }else if(str.substring(0,2).equals(str.substring(str.length()-2,str.length()))){
-//            return str.substring(2, str.length());
-//        }else {
-//            return str;
-//        }
-//    }
+    public String without2(String str) {
+        if(str.length()==0 || str.length()==2) {
+            return "";
+        }else if (str.length()==1){
+            return str;
+        }else if(str.substring(0,2).equals(str.substring(str.length()-2,str.length()))){
+            return str.substring(2, str.length());
+        }else {
+            return str;
+        }
+    }
 
-    //TODO - Strange error on website
+
     public static void main(String[] args) {
         Without2 without2 = new Without2();
 
-//        System.out.println(without2.without2("HelloHe"));
-//        System.out.println(without2.without2("HelloHi"));
-//        System.out.println(without2.without2("Hi"));
-//        System.out.println(without2.without2("x"));
-
-        String str = "x";
-
-        String str1 = str.substring(0,2);
-        String str2 = str.substring(str.length()-2,str.length());
-
-        String str3 = str.substring(2,str.length());
-
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(str3);
-
-        if(str.length()<=2){
-            System.out.println("empty");
-        }else if(str.substring(0,2).equals(str.substring(str.length()-2,str.length()))){
-            System.out.println(str.substring(2, str.length()));
-        }else {
-            System.out.println(str);
-        }
+        System.out.println(without2.without2("HelloHe"));
+        System.out.println(without2.without2("HelloHi"));
+        System.out.println(without2.without2(""));
+        System.out.println(without2.without2("Hi"));
+        System.out.println(without2.without2("x"));
 
     }
 }
